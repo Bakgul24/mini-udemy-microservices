bd2cff17abd71eda90fd0b41a11556a5bd8c1eac
bakgul7 <bnakgul7@gmail.com>
2026-04-10 23:39:25 +0300

    add Eureka Server and API Gateway for microservice orchestration

--- EnrollmentServiceApp/EnrollmentPostService/src/main/java/org/berak/app/service/miniedu/enrollment/service/EnrollmentPostServiceApp.java
+++ EnrollmentServiceApp/EnrollmentPostService/src/main/java/org/berak/app/service/miniedu/enrollment/service/EnrollmentPostServiceApp.java
@@ -2,7 +2,7 @@ package org.berak.app.service.miniedu.enrollment.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
